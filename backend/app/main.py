9293bf12c29be0c66515bb0b81c4286d9a036a9c
Abinanthan K <140876881+abinanthan-k@users.noreply.github.com>
2025-02-24 18:08:29 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -42,5 +42,5 @@ def process_pdf():
     return jsonify({"Summary": result, "Done in": ans})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
 # host=os.getenv('HOST', '0.0.0.0'), port=int(os.getenv('PORT', 8080)),
